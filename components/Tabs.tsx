27ad0d7ea96e612c3f01ee6f6ea0b747ccb1ce39
akshithP <apat0017@student.monash.edu>
2022-05-17 23:12:54 +1000

    Adding badge component for tabs

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -3,126 +3,160 @@ import Tabss from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Badge from "@mui/material/Badge";
+import styles from "./Tabs.module.css";
 
 import { makeStyles } from "@mui/styles";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Props } from "./Row";
 
 interface TabPanelProps {
-	children?: React.ReactNode;
-	index: number;
-	value: number;
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
 // Using createTheme for customising Tabs text
 const useStyles = makeStyles((theme) => ({
-	indicator: {
-		backgroundColor: "black",
-		height: "10px",
-		top: "45px",
-		color: "black",
-	},
+  indicator: {
+    backgroundColor: "black",
+    height: "10px",
+    top: "45px",
+    color: "black",
+  },
 }));
 
 //Using createTheme for customising Tabs text colors
 const theme = createTheme({
-	components: {
-		MuiTab: {
-			styleOverrides: {
-				textColorPrimary: {
-					"&.Mui-selected": {
-						color: "#000000",
-					},
-				},
-				root: {
-					textTransform: "none",
-					fontWeight: "bold",
-					fontFamily: "Noto Sans, sans-serif",
-				},
-				textColorSecondary: {
-					color: "#eeeee4",
-					textTransform: "none",
-					fontWeight: "normal",
-				},
-			},
-		},
-	},
+  components: {
+    MuiTab: {
+      styleOverrides: {
+        textColorPrimary: {
+          "&.Mui-selected": {
+            color: "#000000",
+          },
+        },
+        root: {
+          textTransform: "none",
+          fontWeight: "bold",
+          fontFamily: "Noto Sans, sans-serif",
+          width: "12%",
+        },
+        textColorSecondary: {
+          color: "#eeeee4",
+          textTransform: "none",
+          fontWeight: "normal",
+        },
+      },
+    },
+    MuiBadge: {
+      styleOverrides: {
+        badge: {
+          backgroundColor: "black",
+          right: "-14px",
+          top: "8px",
+        },
+      },
+    },
+  },
 });
 
 const TabPanel = (props: TabPanelProps) => {
-	const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-	return (
-		<div
-			role="tabpanel"
-			hidden={value !== index}
-			id={`simple-tabpanel-${index}`}
-			aria-labelledby={`simple-tab-${index}`}
-			{...other}
-		>
-			{value === index && (
-				<Box sx={{ p: 3 }}>
-					<Typography>{children}</Typography>
-				</Box>
-			)}
-		</div>
-	);
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 };
 
 const Tabs = (props: Props) => {
-	// Creates the tab menu & displays the internal/external data
-	const internal = props.subRows.internal
-	const external = props.subRows.external
-	const user = props.subRows.user
-	const final = props.subRows.final
-	var disableInternal = false
-	var disableExternal = false
+  // Creates the tab menu & displays the internal/external data
+  const internal = props.subRows.internal;
+  const external = props.subRows.external;
+  const user = props.subRows.user;
+  const final = props.subRows.final;
 
-	const [tabVal, setTabVal] = useState(0);
-	const classes = useStyles();
-	const handleChange = (event: SyntheticEvent, newValue: number) => {
-		setTabVal(newValue);
-	};
+  const [tabVal, setTabVal] = useState(0);
+  const classes = useStyles();
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    setTabVal(newValue);
+  };
 
-	let tabLabels = [
-		<Tab key="internal" label="Internal" />,
-		<Tab key="external" label="External" />];
+  let tabLabels = [
+    <Tab
+      key="internal"
+      label={
+        <Badge badgeContent={internal.length} color="primary" showZero>
+          Internal
+        </Badge>
+      }
+    />,
+    <Tab
+      key="external"
+      label={
+        <Badge badgeContent={external.length} color="primary" showZero>
+          External
+        </Badge>
+      }
+    />,
+  ];
 
-	let tabPanels = [
-		<TabPanel key="internal" value={tabVal} index={0}>
-			{internal.length > 0 ? internal : "No depedencies found"}
-		</TabPanel>,
-		<TabPanel key="external" value={tabVal} index={1}>
-			{external.length > 0 ? external : "No depedencies found"}
-		</TabPanel>
-	]
+  let tabPanels = [
+    <TabPanel key="internal" value={tabVal} index={0}>
+      {internal.length > 0 ? internal : "No depedencies found"}
+    </TabPanel>,
+    <TabPanel key="external" value={tabVal} index={1}>
+      {external.length > 0 ? external : "No depedencies found"}
+    </TabPanel>,
+  ];
 
-	if(!final){
-		tabLabels.push(<Tab key="users" label="Users"/>)
-		tabPanels.push(
-			<TabPanel key="users" value={tabVal} index={2}>
-				{user}
-			</TabPanel>
-		)
-	}
+  if (!final) {
+    tabLabels.push(
+      <Tab
+        key="users"
+        label={
+          <Badge badgeContent={user.length} color="primary" showZero>
+            Users
+          </Badge>
+        }
+      />
+    );
+    tabPanels.push(
+      <TabPanel key="users" value={tabVal} index={2}>
+        {user}
+      </TabPanel>
+    );
+  }
 
-	return (
-		<Box sx={{ width: "100%" }}>
-			<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-				<ThemeProvider theme={theme}>
-					<Tabss
-						value={tabVal}
-						onChange={handleChange}
-						aria-label="basic tabs example"
-						TabIndicatorProps={{ className: classes.indicator }}
-					>
-						{tabLabels}
-					</Tabss>
-				</ThemeProvider>
-			</Box>
-			{tabPanels}
-		</Box>
-	);
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <ThemeProvider theme={theme}>
+          <Tabss
+            value={tabVal}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+            TabIndicatorProps={{ className: classes.indicator }}
+          >
+            {tabLabels}
+          </Tabss>
+        </ThemeProvider>
+      </Box>
+      {tabPanels}
+    </Box>
+  );
 };
 
 export default Tabs;
