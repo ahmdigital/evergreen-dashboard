381fd92f991c8d9fede10a4ce27d1d5c4641bbff
MT <matt.git@protonmail.com>
2022-08-24 20:34:29 +1000

    Docker and azure terraform deployment
    * dockerised the application
    * terraform code to automatically deploy the container to azure from docker hub
    * enabled the app to be deployed as a standalone
    * refactored dependencies

---
+++ terraform_azure_docker/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.19.0"
+    }
+  }
+  cloud {
+    organization = "example-org-76fbff"
+
+    workspaces {
+      name = "evergreen-azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  build_imge_tag = "latest"
+  docker_image   = "docker.io/mtempty/evergreendashboard"
+}
+
+
+resource "azurerm_resource_group" "this" {
+  name     = var.app_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "this" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku_name            = var.instance_type.sku_name
+  os_type             = var.instance_type.os_type
+}
+
+resource "azurerm_linux_web_app" "this" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_service_plan.this.location
+  service_plan_id     = azurerm_service_plan.this.id
+  app_settings = {
+    PORT = var.port
+  }
+  site_config {
+    application_stack {
+      docker_image     = local.docker_image
+      docker_image_tag = local.build_imge_tag
+    }
+    always_on     = var.instance_type.sku_name != "F1" ? true : false
+    http2_enabled = true
+  }
+}
