6952d085ed24d353b371d8c7dbc5934417ec3801
alexdelicato <adel0011@student.monash.edu>
2022-08-25 14:40:14 +1000

    added tooltip to help guide fab

--- components/HelpGuide.tsx
+++ components/HelpGuide.tsx
@@ -1,14 +1,32 @@
-import Fab from "@mui/material/Fab";
+import * as React from 'react';
+import { Fab } from "@mui/material";
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
 import HelpOutline from "@mui/icons-material/HelpOutline";
 import styles from "./HelpGuide.module.css";
+import { styled } from '@mui/material/styles';
+
+// defining style for custom tooltip
+const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: '#8F8F8F',
+      color: '#FFFFFF',
+      boxShadow: theme.shadows[1],
+      fontSize: 16,
+      fontFamily: 'Work Sans',
+    },
+}));
 
 
 export default function HelpGuide() {
     return (
       <>
-        <Fab className={styles.fabStyle}>
-            <HelpOutline className={styles.iconStyle}/>
-        </Fab>
+        <LightTooltip title="Help Guide">
+            <Fab className={styles.fabStyle}>
+                <HelpOutline className={styles.iconStyle}/>
+            </Fab>
+        </LightTooltip>
       </>
     );
   }
