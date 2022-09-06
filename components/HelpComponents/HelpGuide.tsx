66005bf2ae841a024eb9c87d4e0b3ddbf4604ca3
akshithP <apat0017@student.monash.edu>
2022-09-07 00:15:18 +1000

    Customized help guide fab using themeProvider

--- components/HelpComponents/HelpGuide.tsx
+++ components/HelpComponents/HelpGuide.tsx
@@ -11,19 +11,41 @@ import yellowIcon from "../images/yellowIcon.svg";
 import redIcon from "../images/redIcon.svg";
 import Image from "next/image";
 import Divider from '@mui/material/Divider';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+// Customising the table styling using ThemeProvider
+const theme = createTheme({
+  components: {
+    MuiFab: {
+      styleOverrides: {
+        root: {
+          display: 'inline-flex',
+          alignItems: 'center',
+          boxSizing: 'border-box',
+          cursor: 'pointer',
+          position: 'fixed',
+          bottom: '24px',
+          right: '24px',
+          zIndex: "10",
+          backgroundColor: '#3B5DD9',
+          color: 'white',
+        }
+      }
+    }
+  }
+})
 
 // defining style for custom tooltip
 const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: '#8F8F8F',
-      color: '#FFFFFF',
-      boxShadow: theme.shadows[1],
-      fontSize: 16,
-      fontFamily: 'Work Sans',
-    },
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#8F8F8F',
+    color: '#FFFFFF',
+    boxShadow: theme.shadows[1],
+    fontSize: 16,
+    fontFamily: 'Work Sans',
+  },
 }));
 
 export interface DialogTitleProps {
@@ -155,17 +177,17 @@ function TabsTable() {
           <TableRow>
             <TableCell className={styles.fieldContentStyle}>Internal:</TableCell>
             <TableCell className={styles.infoContentStyle}>Displays list of internal libraries used by the main repository,
-            and their properties including status, library, used version and the latest version of the library</TableCell>
+              and their properties including status, library, used version and the latest version of the library</TableCell>
           </TableRow>
           <TableRow>
             <TableCell className={styles.fieldContentStyle}>External:</TableCell>
             <TableCell className={styles.infoContentStyle}>Displays list of external libraries used by main repository,
-            and their properties including status, library, used version and the latest version of the library</TableCell>
+              and their properties including status, library, used version and the latest version of the library</TableCell>
           </TableRow>
           <TableRow>
             <TableCell className={styles.fieldContentStyle}>Users:</TableCell>
             <TableCell className={styles.infoContentStyle}>Displays list of libraries that uses the main repository,
-            and their properties including status, library, used version and the latest version of the library</TableCell>
+              and their properties including status, library, used version and the latest version of the library</TableCell>
           </TableRow>
         </TableBody>
       </Table>
@@ -175,82 +197,84 @@ function TabsTable() {
 
 // putting components together for help guide
 export default function HelpGuide() {
-    // State for opening the help guide
-    const [open, setOpen] = React.useState(false);
+  // State for opening the help guide
+  const [open, setOpen] = React.useState(false);
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-      setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-      <>
-        <LightTooltip arrow title="Help Guide">
-            <Fab className={styles.fabStyle} onClick={handleClickOpen}>
-                <HelpOutline className={styles.iconStyle}/>
-            </Fab>
-        </LightTooltip>
-        <Dialog
-            open={open}
-            onClose={handleClose}
-            fullWidth
-            maxWidth="lg"
-        >
-            <CustomisedDialogTitle id="customised-dialog-title" onClose={handleClose}>Need Help?</CustomisedDialogTitle>
-            <DialogContent dividers>
-              <div className={styles.componentStyle}>
-              <DialogContentText className={styles.overallHeaderStyle}>Summary</DialogContentText>
-              <Divider />
-              <DialogContentText className={styles.questionHeaderStyle}>
-                What do the light status icons represent?
-              </DialogContentText>
-              <DialogContentText className={styles.infoContentStyle}>
-                The application uses 3 types of icons to represent how up-to-date a repository is.
-                The status icon is generated based on the semantic versioning of the libraries.
-                Below is a more detailed descripton of each icon:
-              </DialogContentText>
-              <StatusTable />
-              <DialogContentText className={styles.questionHeaderStyle}>
-                What is Overall Percentage?
-              </DialogContentText>
-              <DialogContentText className={styles.infoContentStyle}>
-                The overall percentage represents the number of repositories with a green light
-                status over the total repositories in the organisation as a percentage.
-              </DialogContentText>
-              <DialogContentText className={styles.questionHeaderStyle}>
-                How to read the Total Repositories breakdown card?
-              </DialogContentText>
-              <DialogContentText className={styles.infoContentStyle}>
-                The Total Repos card simply displays the number of repositories defined by each
-                light status type; red, yellow, and green.
-              </DialogContentText>
-              </div>
-              <div className={styles.componentStyle}>
-              <DialogContentText className={styles.overallHeaderStyle}>Repositories </DialogContentText>
-              <Divider />
-              <DialogContentText className={styles.questionHeaderStyle}>
-                Introduction
-              </DialogContentText>
-              <DialogContentText className={styles.infoContentStyle}>
-                The core feature of the application is to keep track of repositories and how-up-to
-                date the internal libraries are. The repositories section displays a table of
-                collapsible rows, where each row represents a repository can be expanded to view
-                its internal libraries.
-              </DialogContentText>
-              <DialogContentText className={styles.questionHeaderStyle}>
-                Repository Table Fields
-              </DialogContentText>
-              <FieldsTable />
-              <DialogContentText className={styles.questionHeaderStyle}>
-                What are the Internal, External and Users tabs?
-              </DialogContentText>
-              <TabsTable />
-              </div>
-            </DialogContent>
-        </Dialog>
-      </>
-    );
-  }
+  return (
+    <>
+      <LightTooltip arrow title="Help Guide">
+        <ThemeProvider theme={theme}>
+          <Fab onClick={handleClickOpen}>
+            <HelpOutline className={styles.iconStyle} />
+          </Fab>
+        </ThemeProvider>
+      </LightTooltip>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        fullWidth
+        maxWidth="lg"
+      >
+        <CustomisedDialogTitle id="customised-dialog-title" onClose={handleClose}>Need Help?</CustomisedDialogTitle>
+        <DialogContent dividers>
+          <div className={styles.componentStyle}>
+            <DialogContentText className={styles.overallHeaderStyle}>Summary</DialogContentText>
+            <Divider />
+            <DialogContentText className={styles.questionHeaderStyle}>
+              What do the light status icons represent?
+            </DialogContentText>
+            <DialogContentText className={styles.infoContentStyle}>
+              The application uses 3 types of icons to represent how up-to-date a repository is.
+              The status icon is generated based on the semantic versioning of the libraries.
+              Below is a more detailed descripton of each icon:
+            </DialogContentText>
+            <StatusTable />
+            <DialogContentText className={styles.questionHeaderStyle}>
+              What is Overall Percentage?
+            </DialogContentText>
+            <DialogContentText className={styles.infoContentStyle}>
+              The overall percentage represents the number of repositories with a green light
+              status over the total repositories in the organisation as a percentage.
+            </DialogContentText>
+            <DialogContentText className={styles.questionHeaderStyle}>
+              How to read the Total Repositories breakdown card?
+            </DialogContentText>
+            <DialogContentText className={styles.infoContentStyle}>
+              The Total Repos card simply displays the number of repositories defined by each
+              light status type; red, yellow, and green.
+            </DialogContentText>
+          </div>
+          <div className={styles.componentStyle}>
+            <DialogContentText className={styles.overallHeaderStyle}>Repositories </DialogContentText>
+            <Divider />
+            <DialogContentText className={styles.questionHeaderStyle}>
+              Introduction
+            </DialogContentText>
+            <DialogContentText className={styles.infoContentStyle}>
+              The core feature of the application is to keep track of repositories and how-up-to
+              date the internal libraries are. The repositories section displays a table of
+              collapsible rows, where each row represents a repository can be expanded to view
+              its internal libraries.
+            </DialogContentText>
+            <DialogContentText className={styles.questionHeaderStyle}>
+              Repository Table Fields
+            </DialogContentText>
+            <FieldsTable />
+            <DialogContentText className={styles.questionHeaderStyle}>
+              What are the Internal, External and Users tabs?
+            </DialogContentText>
+            <TabsTable />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
