7d2128267c32c03f29af07a69e6e20f4d54e545c
akshithP <apat0017@student.monash.edu>
2022-09-08 19:50:26 +1000

    Changed customisation of tableCell in reposOverviewTable

--- components/SummaryComponents/RepoOverviewTable/ReposOverviewTable.tsx
+++ components/SummaryComponents/RepoOverviewTable/ReposOverviewTable.tsx
@@ -7,6 +7,7 @@ import YellowIcon from "../../../components/images/yellowIcon.svg";
 import greenIcon from "../../../components/images/greenIcon.svg";
 import Image from "next/image";
 import { TableBody } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 type RowProps = {
   icon: any;
@@ -14,6 +15,22 @@ type RowProps = {
   statusCount: number;
   statusLabel: string;
 };
+
+const theme = createTheme({
+  components: {
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          fontSize: 'var(--font-size-large)',
+          fontWeight: 'var(--font-weight-semibold)',
+          marginTop: 0,
+          marginBottom: 0,
+          padding: '16px 10px 16px 10px',
+        }
+      }
+    },
+  }
+})
 
 enum StatusLabel {
   RED = "need(s) updating urgently",
@@ -25,20 +42,22 @@ const Row = (props: RowProps) => {
   const ICON_SIZE = "40px";
   return (
     <TableRow>
-      <TableCell className={styles.tableCellStyleIcon}>
-        <Image
-          src={props.icon}
-          alt={props.iconAlt ? props.iconAlt : ""}
-          width={ICON_SIZE}
-          height={ICON_SIZE}
-        ></Image>
-      </TableCell>
-      <TableCell className={styles.tableCellStyleCount}>
-        <p>{props.statusCount} </p>
-      </TableCell>
-      <TableCell className={styles.statusLabel}>
-        <p>{props.statusLabel}</p>
-      </TableCell>
+      <ThemeProvider theme={theme}>
+        <TableCell className={styles.tableCellStyleIcon}>
+          <Image
+            src={props.icon}
+            alt={props.iconAlt ? props.iconAlt : ""}
+            width={ICON_SIZE}
+            height={ICON_SIZE}
+          ></Image>
+        </TableCell>
+        <TableCell className={styles.tableCellStyleCount}>
+          <p>{props.statusCount} </p>
+        </TableCell>
+        <TableCell className={styles.statusLabel}>
+          <p>{props.statusLabel}</p>
+        </TableCell>
+      </ThemeProvider>
     </TableRow>
   );
 };
