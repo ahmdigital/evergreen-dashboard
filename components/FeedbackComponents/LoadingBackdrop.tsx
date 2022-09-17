e6c82951b7bd93654e9d8fee60b45d99d37b6ade
alexdelicato <adel0011@student.monash.edu>
2022-09-17 14:53:45 +1000

    initial changes for loading backdrop to snackbar

--- components/FeedbackComponents/LoadingBackdrop.tsx
+++ components/FeedbackComponents/LoadingBackdrop.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Backdrop from '@mui/material/Backdrop';
+import Snackbar from '@mui/material/Snackbar';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import CircularProgress, { circularProgressClasses } from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
+import styles from "../../styles/LoadingBackdrop.module.css";
+import { blue } from '@mui/material/colors';
 
 
 // personalised linear progress bar
+/* 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 20,
   borderRadius: 5,
@@ -17,8 +22,9 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
   },
 }));
-
+*/
 // backdrop for when app is updating data
+/* 
 export default function LoadingBackdrop(props: {
     open: boolean;
 }) {
@@ -39,6 +45,39 @@ export default function LoadingBackdrop(props: {
             <p>Fetching Latest Updates...</p>
         </Box>
       </Backdrop>
+    </div>
+  );
+}
+*/
+
+export default function LoadingBackdrop(props: {
+    open: boolean;
+}) {
+  return (
+    <div className={styles.snackbar}>
+      <Snackbar
+      open={props.open}
+      sx={{ 
+        backgroundColor: 'rgb(59,93,214)',
+        borderRadius: 4,
+        width: 350,
+        alignItems: 'flex-end',
+        boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)'
+      }}
+      >
+        <Box sx={{ width: '80%' , alignItems: 'flex', padding: '1rem'}}>
+            <CircularProgress 
+
+              sx={{
+                color: (theme) =>
+                  theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+              }}
+              size={40}
+              thickness={5}
+            />
+            <p className={styles.p}>Fetching Latest Updates...</p>
+        </Box>
+      </Snackbar>
     </div>
   );
 }
