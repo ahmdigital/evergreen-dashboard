5b82efb3ee88c0ab210548b28c202255fb55c6f0
akshithP <apat0017@student.monash.edu>
2022-09-23 15:53:40 +1000

    Implemented BG image and styled auth page with @ChristianMarchetta

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router"
-import { Button, CircularProgress, Grid, Paper, Typography, Link, Box, Alert, AlertTitle } from "@mui/material";
+import { Button, CircularProgress, Grid, Stack, Paper, Typography, Link, Box, Alert, AlertTitle } from "@mui/material";
 import styles from "../styles/signin.module.css";
 import { GitHubIcon } from "../components/GitHubIcon";
 import * as React from 'react';
 import LoadingButton from '@mui/lab/LoadingButton';
 import TrafficIcon from '@mui/icons-material/Traffic';
+import authBG from '../components/images/authPageBG.jpg'
+import Image from "next/image";
+import ForestIcon from "@mui/icons-material/Forest";
 
 
 const SCOPE = "repo"
@@ -85,81 +88,131 @@ export default function SignIn() {
 	return (
 		<>
 			{/* <button onClick={redirect}>Login</button> */}
-			<Grid container>
-				<Grid item xs={0} md={5} sx={{ heigth: '100%', backgroundColor: 'green' }}>
+			<Grid container alignItems="stretch" sx={{
+				height: '100vh'
+			}}>
+				<Grid item xs={12} md={5} sx={{
+					backgroundColor: 'green',
+					backgroundImage: 'url("/authPageBG.jpg")',
+					backgroundSize: 'cover',
+					backgroundPositionX: '80%',
+					position: 'relative',
+					overflow: 'hidden'
+
+				}}>
+
+					<Box sx={{ padding: 4 }}>
+						<Typography sx={{ fontWeight: 'bold' }} variant="h4">Evergreen <ForestIcon /> </Typography>
+						<Typography variant="body1">
+							Evergreen lets you track all your dependencies in one simple dashboard.
+						</Typography>
+					</Box>
+
+					{/* <Grid justifyItems={"flex-end"} spacing={0} alignItems="stretch"> */}
+					<Box sx={{
+						position: 'absolute',
+						bottom: '0',
+						left: '75px',
+						width: '120%',
+						display: {
+							xs: 'none',
+							md: 'initial'
+						}
+					}}>
+
+						<Image
+							src='/dashboardSS.png'
+							alt="Screenshot of the Evergreen Dashboard"
+							width={2692}
+							height={2048} />
+						{/* </Grid> */}
+					</Box>
 
 				</Grid>
-				<Grid item xs={12} md={7}>
+			</Grid>
 
-					<Grid container className={styles.signinPage} justifyContent='center' alignContent='center'>
-						<Paper className={styles.signinContainer}>
-							{/* TODO: evergreen dashborad logo, we are aming for this design https://gitter.im/ */}
-							<Typography variant="h4" sx={{ marginBottom: '1.5rem' }} component="h1"><span className={styles.everGreen}>Evergreen Dashboard</span> </Typography>
-							{/* TODO: add documentation URL */}
-							<Typography variant="body1" sx={{ fontWeight: 'var(--font-weight-semibold)' }}>You can read <Link href="#">our documentation</Link> on OAuth scopes to see why we request certain OAuth scopes.</Typography>
+			<Grid
+				sx={{
+					position: 'fixed',
+					top: '50%',
+					left: {
+						xs: '50%',
+						md: '70.83%'
+					},
+					transform: 'translate(-50%, -50%)',
+					maxWidth: '100%',
+					padding: 4
+				}}
+				container
+				className={styles.signinPage}
+				justifyContent='center'
+				alignContent='center'>
+				<Paper className={styles.signinContainer}>
+					{/* TODO: evergreen dashborad logo, we are aming for this design https://gitter.im/ */}
+					<Typography variant="h4" sx={{ marginBottom: '1.5rem' }} component="h1"><span className={styles.everGreen}>Evergreen Dashboard</span> </Typography>
+					{/* TODO: add documentation URL */}
+					<Typography variant="body1" sx={{ fontWeight: 'var(--font-weight-semibold)' }}>You can read <Link href="#">our documentation</Link> on OAuth scopes to see why we request certain OAuth scopes.</Typography>
 
-							{
-								signInStatus === "calculating" && <>
-									<Box sx={{ py: 10 }}>
-										<CircularProgress />
-									</Box>
-								</>
-							}
-							{
-								signInStatus === "not-signed-in" && <>
+					{
+						signInStatus === "calculating" && <>
+							<Box sx={{ py: 10 }}>
+								<CircularProgress />
+							</Box>
+						</>
+					}
+					{
+						signInStatus === "not-signed-in" && <>
 
-									<Box sx={{ py: 10 }}>
-										<Button
-											variant="contained"
-											size="large"
-											className={styles.signinWithGithubButton}
-											onClick={redirect}
-											startIcon={<GitHubIcon />}
-										>Sign In With GitHub</Button>
-									</Box>
-								</>
-							}
-							{
-								signInStatus === "exchanging-code-for-token" && <>
-									<Box sx={{ py: 5 }}>
-										<LoadingButton
-											loading
+							<Box sx={{ py: 10 }}>
+								<Button
+									variant="contained"
+									size="large"
+									className={styles.signinWithGithubButton}
+									onClick={redirect}
+									startIcon={<GitHubIcon />}
+								>Sign In With GitHub</Button>
+							</Box>
+						</>
+					}
+					{
+						signInStatus === "exchanging-code-for-token" && <>
+							<Box sx={{ py: 5 }}>
+								<LoadingButton
+									loading
 
-											variant="outlined"
-										>
-											Almost done
-										</LoadingButton>
-									</Box>
-								</>
-							}
-							{
-								signInStatus === "error-while-signing-in" && <>
-									<Alert severity="error">
-										<AlertTitle>Error</AlertTitle>
-										{
-											errorMessage
-												? errorMessage
-												: "Whoops! There was an error while trying to sign you in."
-										}
-									</Alert>
-									<Button onClick={handleTryAgain}>Try Again</Button>
-								</>
-							}
-							{
-								signInStatus === "signed-in" && <>
-									<Alert severity="success">
-										<AlertTitle>Hi { }</AlertTitle>
-										You have successfully signed in!
-									</Alert>
-									<Button variant="contained"
-										size="large"
-										endIcon={<TrafficIcon />}
-										href="/">Continue to the dashboard</Button>
-								</>
-							}
-						</Paper>
-					</Grid>
-				</Grid>
+									variant="outlined"
+								>
+									Almost done
+								</LoadingButton>
+							</Box>
+						</>
+					}
+					{
+						signInStatus === "error-while-signing-in" && <>
+							<Alert severity="error">
+								<AlertTitle>Error</AlertTitle>
+								{
+									errorMessage
+										? errorMessage
+										: "Whoops! There was an error while trying to sign you in."
+								}
+							</Alert>
+							<Button onClick={handleTryAgain}>Try Again</Button>
+						</>
+					}
+					{
+						signInStatus === "signed-in" && <>
+							<Alert severity="success">
+								<AlertTitle>Hi { }</AlertTitle>
+								You have successfully signed in!
+							</Alert>
+							<Button variant="contained"
+								size="large"
+								endIcon={<TrafficIcon />}
+								href="/">Continue to the dashboard</Button>
+						</>
+					}
+				</Paper>
 
 			</Grid>
 		</>
