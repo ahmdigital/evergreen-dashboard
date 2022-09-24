b9011c33f1d9f10dde940cf1f5d5efd7d10817a5
Christian <cmar0027@student.monash.edu>
2022-09-24 14:59:19 +1000

    Recreating the dependencies table

---
+++ components/SemVerFormatter.tsx
@@ -0,0 +1,25 @@
+import { SemVer, semVerToString } from "../src/semVer"
+import QuestionMark from "@mui/icons-material/QuestionMark";
+import { Tooltip } from "@mui/material";
+
+type SemVerProps = {
+	semver: SemVer
+}
+
+export function SemVerFormatter(props: SemVerProps) {
+	const formatted = semVerToString(props.semver)
+	return <>
+		{formatted}
+		{
+			(formatted === "0.0.0-development" || formatted === "0.0.0") &&
+			<Tooltip arrow title={
+				<p style={{fontSize: 'var(--font-size-normal)', fontFamily: 'var(--primary-font-family)'}}>
+				This repository was defined with a default version of 0.0.0
+				</p>
+			}>
+				<QuestionMark sx={{ width: '1.125rem', height: '1.125rem' }} />
+			</Tooltip>
+			
+		}
+		</>
+}
