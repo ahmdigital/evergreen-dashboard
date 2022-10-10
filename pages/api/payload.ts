fef770653cbb8ac26aa2122118bde55d88bb8dc9
MT <matt.git@protonmail.com>
2022-10-11 00:45:57 +1100

    fixed issue with with ref name

--- pages/api/payload.ts
+++ pages/api/payload.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { handleGitHubWebhookPushEvents, handleGitHubWebhookRepositoryEvents } from "../../node_modules/evergreen-org-crawler/src/webhooks/github";
+// import { handleGitHubWebhookPushEvents, handleGitHubWebhookRepositoryEvents } from "evergreen-org-crawler";
 
 import getConfig from "next/config";
 const { publicRuntimeConfig: config } = getConfig();
@@ -21,51 +22,48 @@ export default async function handler(
 	res: NextApiResponse
 ) {
 	const eventType: string = req.headers["x-github-event"] as string
-	if (Object.values(acceptedEventTypes).includes(eventType as acceptedEventTypes) && req.body != null) {
-		try {
-			if (req.body.organization.login ==
-				process.env.NEXT_PUBLIC_TARGET_ORGANISATION) {
-				// Resolve as quickly as possible
-				res.status(200).end()
+	try {
+		if (Object.values<string>(acceptedEventTypes).includes(eventType) && req.body != null && req.body.organization.login ==
+			process.env.NEXT_PUBLIC_TARGET_ORGANISATION) {
+			// Resolve as quickly as possible
+			res.status(200).end()
 
-				// skip any push event that doesn't target the default branch
-				// This is temporary, until we delegate this to crawler lib
-				if (eventType == acceptedEventTypes.PUSH && req.body.ref != req.body.repository.default_branch) {
+			// skip any push event that doesn't target the default branch
+			// This is temporary, until we delegate this to crawler lib
+			if (eventType == acceptedEventTypes.PUSH && req.body.ref != "refs/heads/" + req.body.repository.default_branch) {
+				return
+			}
+
+			if (waitingPromise.promise != null) {
+				if (alreadyOneWaiting) {
+					// we only have to buffer a single request at a time, running other will be repetitive work
 					return
 				}
+				// if alreadyOneWaiting is not used, all waiting promises here will proceed to execute as soon the promise is resolved
 
-				if (waitingPromise.promise != null) {
-					if (alreadyOneWaiting) {
-						// we only have to buffer a single request at a time, running other will be repetitive work
-						return
-					}
-					// if alreadyOneWaiting is not used, all waiting promises here will proceed to execute as soon the promise is resolved
+				console.log("Someone else has the promise, I will wait for them finish");
 
-					console.log("Someone else has the promise, I will wait for them finish");
+				alreadyOneWaiting = true
+				await waitingPromise.promise;
+				alreadyOneWaiting = false
 
-					alreadyOneWaiting = true
-					await waitingPromise.promise;
-					alreadyOneWaiting = false
-
-					// waitingPromise.promise = null;
-				}
-				console.log(
-					"I'm now free now to call backend at the moment, And I will create a new promise"
-				);
-				waitingPromise.promise = new Promise(function (resolve, reject) {
-					waitingPromise.resolve = resolve;
-				});
-
-				updateCache(req.body, eventType);
+				// waitingPromise.promise = null;
 			}
-			else {
-				res.status(404).end()
-			}
+			console.log(
+				"I'm now free now to call backend at the moment, And I will create a new promise"
+			);
+			waitingPromise.promise = new Promise(function (resolve, reject) {
+				waitingPromise.resolve = resolve;
+			});
 
-		} catch (error) {
-			console.log(`Failed to process GitHub event cache: ${error}`);
-			res.status(500).end()
+			updateCache(req.body, eventType);
 		}
+		else {
+			res.status(404).end()
+		}
+	} catch (error) {
+		console.log(`Failed to process GitHub event cache: ${error}`);
+		res.status(500).end()
 	}
 }
 
